6cd9d2fd6ab2f50d65321740fca5675260ffac9f
Maotou <93722462+maotou-spy@users.noreply.github.com>
2023-11-02 14:10:43 +0700

    init Studio detail

---
+++ TatooLover/TattooRazorPages/Pages/StudioPage/Details.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TattooRazorPages.Pages.StudioPage
+{
+    public class DetailsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
