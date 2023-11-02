01fe632e2dda5f6013a97631921a0edaef1bbe7b
Maotou <93722462+maotou-spy@users.noreply.github.com>
2023-11-02 14:09:13 +0700

    init details

--- TatooLover/TattooRazorPages/Pages/Admin/AdminHome.cshtml.cs
+++ TatooLover/TattooRazorPages/Pages/Admin/AdminHome.cshtml.cs
@@ -1,12 +1,25 @@
+using BusinessObjects.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Repositories;
 
 namespace TattooRazorPages.Pages.Admin
 {
     public class AdminHomeModel : PageModel
     {
+        public IBookingRepository bookingRepository = new BookingRepository();
+        public IStudioRepository studioRepository = new StudioRepository();
+        public ICustomerRepository customerRepository = new CustomerRepository();
+        public IArtistRepository artistRepository = new ArtistRepository();
+
+        public List<Customer> customerList = new List<Customer>();
+        public List<Booking> bookingList = new List<Booking>();
+        public List<Studio> studioList = new List<Studio>();
         public void OnGet()
         {
+            customerList = customerRepository.GetCustomers();
+            bookingList = bookingRepository.GetBookings();
+            studioList = studioRepository.GetStudios();
         }
     }
 }
